4aae08aa06025841733d69fed25b62fdbddfda69
bangnokia <bangnokia@gmail.com>
2022-09-03 22:55:59 +0700

    work in progress

--- src/Resources/Summaries.php
+++ src/Resources/Summaries.php
@@ -4,6 +4,13 @@ namespace BangNokia\WakaTime\Resources;
 
 class Summaries extends Resource
 {
+    public function __construct(array $attributes)
+    {
+        parent::__construct($attributes);
+
+        // custom fill data for this resource
+    }
+
     public array $data;
 
     public array $cummulativeTotal;
